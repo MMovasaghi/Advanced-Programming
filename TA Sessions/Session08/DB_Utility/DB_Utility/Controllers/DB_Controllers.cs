193600e8c2b0dc0a58c14cb88bd0efcfa52e3d37
MH.Movasaghi <mmghho77@gmail.com>
2019-05-14 20:17:59 +0430

    DB_Utility Update

---
+++ TA Sessions/Session08/DB_Utility/DB_Utility/Controllers/DB_Controllers.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DB_Utility.Models;
+
+namespace DB_Utility.Controllers
+{
+    public class DB_Controllers
+    {
+        public bool AddNewProduct(Product product)
+        {
+            try
+            {
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool AddNewPerson(Person person)
+        {
+            try
+            {
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool AddNewShoppingCardToPerson(Person person)
+        {
+            try
+            {
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool AddNewLikeProductToPerson(Person person , LikeProduct likeProduct)
+        {
+            try
+            {
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool EditPerson(Person EditedPerson , string PersonName)
+        {
+            try
+            {
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool EditProduct(Person EditedProduct, string ProductName)
+        {
+            try
+            {
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
