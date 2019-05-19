259f4dc1d60a502a57c6861860acdb2c47ba01be
MH.Movasaghi <mmghho77@gmail.com>
2019-05-19 18:54:33 +0430

    Doc

---
+++ MidTerm/SampleCode/SampleCode/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample<string> sample = new Sample<string>();
+
+            sample.Type = "H";
+            Sample<string> s;
+
+            string a = "HELLO WORLD!";
+            for (int i = 0; i < a.Length ; i++)
+            {
+                s = new Sample<string>();
+                s.Type = a[i].ToString();
+                s.AnotherSAMPLE = new Sample<string>();
+                s = s.AnotherSAMPLE;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MidTerm/SampleCode/SampleCode/SAMPLE.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCode
+{
+    class Sample<T>
+    {
+        public T Type { get; set; }
+        public Sample<T> AnotherSAMPLE { get; set; }        
+    }
+}
